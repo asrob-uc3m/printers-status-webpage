ec113bdab13a22d77d55fff475082451b7381269
David.Estevez <david.estevez.fdez@gmail.com>
2017-04-11 14:30:17 +0200

    Remove dummy sentences from template

--- generate.py
+++ generate.py
@@ -1,14 +1,14 @@
 import begin
+import os
 import requests
 import json
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-@begin.start(auto_convert=True, config_file='config.txt')
+@begin.start(auto_convert=True)
 @begin.logging
-def start():
-
-    # This parts get the data from github
+def start(output_file: 'Select website directory to write index.html'= os.path.join(os.path.split(__file__)[0],'website', 'index.html')):
+    # This parts get the data from GitHub
     url = "https://api.github.com/repos/asrob-uc3m/impresoras-asrob/issues?state=open&labels=NO%20FUNCIONA,{}"
     printers = {'BLACKY': {
                     'name':'Blacky',
@@ -31,13 +31,14 @@ def start():
         for issue in obj:
             printers[printer_name]['issues'].append({'title':issue['title'], 'url':issue['html_url']})
 
-    print(printers)
+    # print(printers)
 
     # This part generates the webpage
     env = Environment(loader=FileSystemLoader('./templates'), autoescape=select_autoescape(['html', 'xml']))
     template = env.get_template('index.html')
     html = template.render(printers=sorted(list(printers.values()), key=lambda x: x['index']))
 
-    with open('./website/index.html', 'w') as f:
+    # Only thing left is to save the rendered template
+    with open(output_file, 'w') as f:
         f.write(html)
 
